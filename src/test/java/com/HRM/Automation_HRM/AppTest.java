22a1bd8e0528f76411a191c24c1e64302ff7152d
santoshkakuluru999@gmail.com <santhoshkakuluru999@gmail.com>
2023-03-21 13:18:12 +0530

    initial commits by santhos

---
+++ src/test/java/com/HRM/Automation_HRM/AppTest.java
@@ -0,0 +1,20 @@
+package com.HRM.Automation_HRM;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
